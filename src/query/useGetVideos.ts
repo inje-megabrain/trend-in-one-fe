ec3348b537e893b2102f1e8e89726a97caa34f64
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2023-01-14 21:50:19 +0900

    fix: video api 수정

--- src/query/useGetVideos.ts
+++ src/query/useGetVideos.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "react-query";
 import getVideos from "../api/getVideos";
 
-const useGetSpecificCommunityPosts = () => {
+const useGetVideos = () => {
     return useQuery(["videos"], getVideos);
 };
 
-export default useGetSpecificCommunityPosts;
+export default useGetVideos;
